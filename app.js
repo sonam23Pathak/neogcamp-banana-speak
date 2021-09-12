783d2529d6c546d489c237d7658317072f1b861f
sonam23Pathak <76949052+sonam23Pathak@users.noreply.github.com>
2021-09-12 16:54:14 +0530

    Taking inputs

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate = document.querySelector("#btnTranslate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log("txtinput")
+btnTranslate.addEventListener("click", function clickEventHandler() {
+     console.log("Clicked!")
+})
+
