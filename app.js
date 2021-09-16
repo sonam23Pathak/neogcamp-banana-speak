acf95a2548610f1dd33dffb8400897966632b381
sonam23Pathak <76949052+sonam23Pathak@users.noreply.github.com>
2021-09-16 11:47:20 +0530

    added input and output

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var btnTranslate = document.querySelector("#btnTranslate");
-var txtInput = document.querySelector("#txt-input");
+const btnTranslate = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txt-input");
+const output = document.querySelector("#output");
 
-console.log("txtinput")
-btnTranslate.addEventListener("click", function clickEventHandler() {
-     console.log("Clicked!")
-})
+const serverURL = "https://api.funtranslations.com/translate/minion.json" + "?text=Hello sonam";
 
+function clickHandler() {
+
+     output.innerText = "hiiii"
+
+
+     fetch(serverURL)
+          .then(response => response.json())
+          .then(data => output.innerText = data.contents.translated)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
